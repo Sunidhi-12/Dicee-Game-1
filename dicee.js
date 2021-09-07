3bfc7de8d7b0dd94a05239fe95b2465d7e67fec4
Sunidhi-12 <90262774+Sunidhi-12@users.noreply.github.com>
2021-09-07 20:44:25 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomImage = "dice"+randomNumber1+".png";
+var img=document.querySelectorAll("img")[0];
+img.setAttribute("src",randomImage);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImage1 = "dice"+randomNumber2+".png";
+var img=document.querySelectorAll("img")[1];
+img.setAttribute("src",randomImage1);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player1 Wins";
+}
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML="Player2 Wins";
+}
+else document.querySelector("h1").innerHTML="Tie";
+
+
